4c5bb45c7664c6d79595737e8adcd6e3654fabe9
NinaOosterlaar <ninaoosterlaar@gmail.com>
2024-05-26 13:49:47 +0200

    id now works

--- ids/app.py
+++ ids/app.py
@@ -1,22 +1,24 @@
 import logging
-from flask import Flask
-from threading import Lock
+from flask import Flask, abort
 from datetime import datetime
+import os
+from pymemcache.client import base
 
-sequence_lock = Lock()
+
 
 app = Flask("ids-service")
 
-
-counter = 0
+client = base.Client(("memcached", 11211))
+client.set("counter", 1)
 
 @app.get('/create')
 def create_id():
-    global counter
+    counter = int(client.get("counter").decode('utf-8'))
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
-    with sequence_lock:
-        counter += 1
-        return f"log:{timestamp}{counter}"
+    counter += 1
+    client.set("counter", counter)
+    return f"log:{timestamp}{counter}"
+  
 
 
 if __name__ == '__main__':
